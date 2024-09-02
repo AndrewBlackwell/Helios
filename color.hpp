f31bceef2402070ed8c3723603277273053b928d
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-02 18:28:38 -0400

    Color utilities complete, modified main for new output

--- color.hpp
+++ color.hpp
@@ -6,4 +6,18 @@
 
 using color = vector; // RGB color
 
+void write_color(std::ostream &out, color pcolor)
+{
+    auto r = pcolor.x();
+    auto g = pcolor.y();
+    auto b = pcolor.z();
+
+    // Translate [0, 1] -> [0, 255]
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << " " << gbyte << " " << bbyte << "\n";
+}
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "color.hpp"
+#include "vector.hpp"
+
 #include <iostream>
 
 int main()
@@ -13,15 +16,21 @@ int main()
         std::clog << "\rLines remaining: " << (height - i) << ' ' << std::flush;
         for (int j = 0; j < width; j++)
         {
-            auto red = double(j) / (height - 1);
-            auto green = double(i) / (width - 1);
-            auto blue = 0.5 * (red + green);
+            // Normalize coordinates to range [0, 1]
+            double x = double(i) / (width - 1);
+            double y = double(j) / (height - 1);
 
-            int ired = int(255.999 * red);
-            int igreen = int(255.999 * green);
-            int iblue = int(255.999 * blue);
+            // Calculate the angle of the point in polar coordinates
+            double angle = std::atan2(y - 0.5, x - 0.5) + M_PI;
 
-            std::cout << ired << " " << igreen << " " << iblue << "\n";
+            // Map angle to a color: 0 to 2π maps to 0 to 1 (normalized)
+            double r = 0.5 + 0.5 * std::cos(angle);
+            double g = 0.5 + 0.5 * std::cos(angle - 2.0 * M_PI / 3.0);
+            double b = 0.5 + 0.5 * std::cos(angle - 4.0 * M_PI / 3.0);
+
+            // Create a color object from these values
+            auto pixel_color = color(r, g, b);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone!                 \n";
