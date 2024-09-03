cc94a75b324a0aecc7c3a6f665ba499081afd180
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-03 13:50:53 -0400

    Defined ray Class

--- ray.hpp
+++ ray.hpp
@@ -1,4 +1,29 @@
 #ifndef RAY_HPP
 #define RAY_HPP
 
+#include "vector.hpp"
+
+class ray
+{
+public:
+    ray() {}
+
+    ray(const point3D &origin, const vector &direction)
+        : orig(origin), dir(direction)
+    {
+    }
+
+    const point3D &origin() const { return orig; }
+    const vector &direction() const { return dir; }
+
+    point3D at(double t) const
+    {
+        return orig + t * dir;
+    }
+
+private:
+    point3D orig;
+    vector dir;
+};
+
 #endif
