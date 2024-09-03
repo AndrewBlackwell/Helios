13a3e5a5d32570c6febeae62bc8e950051805026
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-03 19:07:15 -0400

    `chore: Add common.hpp, hittable_list.hpp, is_hittable.hpp, and sphere.hpp`

---
+++ is_hittable.hpp
@@ -0,0 +1,33 @@
+#ifndef IS_HITTABLE_HPP
+#define IS_HITTABLE_HPP
+
+#include "ray.hpp"
+
+class hit_record
+{
+public:
+    point3D p;
+    vector normal;
+    double t;
+
+    bool front_face;
+
+    void set_face_normal(const ray &r, const vector &outward_normal)
+    {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class is_hittable
+{
+public:
+    virtual ~is_hittable() = default;
+
+    virtual bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const = 0;
+};
+
+#endif
---
+++ sphere.hpp
@@ -0,0 +1,46 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "is_hittable.hpp"
+#include "vector.hpp"
+
+class sphere : public is_hittable
+{
+public:
+    sphere(const point3D &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+    {
+        vector oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
+
+        auto discriminant = h * h - a * c;
+        if (discriminant < 0)
+            return false;
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+        {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vector outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
+private:
+    point3D center;
+    double radius;
+};
+#endif
