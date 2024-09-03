00cc9c4e4cb1dbfa64746b6bead158718be9db55
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-03 19:34:02 -0400

    Include Marsenne Twister for PRNG

--- common.hpp
+++ common.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 // C++ Std Usings
 
@@ -18,9 +19,24 @@ const double pi = 3.1415926535897932385;
 
 // Utility Functions
 
+// Utility Functions
+
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // Returns a random real in [0,1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
 // Common Headers
