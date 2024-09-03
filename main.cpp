56a50e51f6b08d73c340457d03045a68ad355ccc
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-03 18:42:47 -0400

    We can render shapes!

--- main.cpp
+++ main.cpp
@@ -4,8 +4,23 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3D &center, double radius, const ray &r)
+{
+    vector oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray &r)
 {
+    if (hit_sphere(point3D(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
+
     vector unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
