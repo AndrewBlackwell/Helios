510ef3c0efece2c025cd31c22ca6e66b94e7b18d
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-14 20:09:34 -0400

    Refactor camera class to include maximum ray depth

--- camera.hpp
+++ camera.hpp
@@ -11,6 +11,7 @@ public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int width = 100;            // Rendered image width in pixel count
     int samples_per_pixel = 10; // Count of random samples for each pixel
+    int max_depth = 10;         // Maximum number of ray bounces into scene
 
     void render(const is_hittable &world)
     {
@@ -28,7 +29,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; sample++)
                 {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -92,14 +93,18 @@ private:
         return vector(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray &r, const is_hittable &world) const
+    color ray_color(const ray &r, int depth, const is_hittable &world) const
     {
+        // If we've exceeded the ray bounce limit, no more light is gathered.
+        if (depth <= 0)
+            return color(0, 0, 0);
+
         hit_record rec;
 
         if (world.hit(r, interval(0, infinity), rec))
         {
             vector direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vector unit_direction = unit_vector(r.direction());
