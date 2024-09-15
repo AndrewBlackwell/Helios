e570c6da9764492732a1ad262b1c06ef679c95eb
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2024-09-14 22:02:43 -0400

    Lambertian diffusion

--- camera.hpp
+++ camera.hpp
@@ -103,7 +103,7 @@ private:
 
         if (world.hit(r, interval(0.001, infinity), rec))
         {
-            vector direction = random_on_hemisphere(rec.normal);
+            vector direction = rec.normal + random_unit_vector();
             return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
